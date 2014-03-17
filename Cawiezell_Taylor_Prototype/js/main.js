2b92343ff2581bbc69c53653795f42b3c369fae9
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2014-03-16 22:34:10 -0500

    zip file

---
+++ Cawiezell_Taylor_Prototype/js/main.js
@@ -0,0 +1,23 @@
+(function($){
+
+
+
+
+$('#search-button').click(function() {
+    window.location.href = 'result.html';
+    return false;
+});
+
+$('#sign-up-button').click(function() {
+    window.location.href = 'sign-up.html';
+    return false;
+});
+
+
+
+
+
+
+
+
+})(jQuery); // end private scope
