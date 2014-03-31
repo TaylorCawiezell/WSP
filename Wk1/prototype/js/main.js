8487067d71fd98245c3f61fd5222f308d19ce229
TaylorCawiezell <taylorcawiezell@fullsail.edu>
2014-03-30 19:34:26 -0500

    final

--- Wk1/prototype/js/main.js
+++ Wk1/prototype/js/main.js
@@ -26,17 +26,67 @@ $('#sign-up-button').click(function() {
     return false;
 });
 
- $('.games li').append( $( "<div class='rating'><p>"+number.toFixed(1)+"</p></div>" ) );
+$('.games li').append( $( "<div class='rating'><p>"+number.toFixed(1)+"</p></div>" ) );
 
 $( ".games li" ).hover(
   function() {
-    $( this ).append( $( "<img class='favorite-banner' src='img/fav-not-saved.png'>" + "<a href='details.html'><p class='details'>View Details</p></a>").hide().fadeIn(800));
-  }, function() {
+    $( this ).append( $( "<div class='img-toggle'><img class='favorite-not-banner' src='img/fav-not-saved.png' title='Add to favorites'><img class='favorite-banner' src='img/fav-saved.png' title='Add to favorites'></div>" + "<a href='details.html#start'><p class='details'>View Details</p></a>").hide().fadeIn(600));
+  }, 
+  function() {
     $( this ).find( ".favorite-banner" ).fadeOut(100);
     $( this ).find( ".details" ).fadeOut(100);
   }
 );
 
+
+$( ".login p" ).click(function() {
+  $( ".login form" ).toggle( "fast", function() {
+    // Animation complete.
+  });
+});
+
+$( ".sign-in-m" ).click(function() {
+  $( ".buttons form" ).toggle( "fast", function() {
+    // Animation complete.
+  });
+});
+
+$( ".filter" ).click(function() {
+  $( ".filter-select" ).toggle( "fast", function() {
+  });
+});
+
+$( ".filter-select" ).mouseleave(function() {
+  $( this ).fadeOut();
+});
+
+$( ".filter-m" ).click(function() {
+  $( ".filter-select" ).toggle( "fast", function() {
+  });
+});
+
+$( ".filter-select" ).mouseleave(function() {
+  $( this ).fadeOut();
+});
+
+$( ".game-goer" ).mouseleave(function() {
+  $( ".login form" ).fadeOut();
+});
+
+$( ".game-goer" ).hover(
+   function() {
+     $('.login p').slideDown();
+      $('.sign-up').slideDown();
+   },
+   function(){
+   	 $('.login p').fadeOut(200);
+   	 $('.sign-up').fadeOut(200);
+   });
+
+$('.button').click(function() {
+    window.location.href = 'account.html';
+    return false;
+});
 
 
 
